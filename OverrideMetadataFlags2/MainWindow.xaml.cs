7fa704b84d0f2aa6faba858fcf63f19118a2aa93
v-trisshores <86677757+v-trisshores@users.noreply.github.com>
2021-12-02 18:00:21 -0600

    Add OverrideFrameworkMetadataFlags project

--- OverrideMetadataFlags2/MainWindow.xaml.cs
+++ OverrideMetadataFlags2/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls.Primitives;
 
@@ -17,21 +18,73 @@ namespace WpfApp1
             TestControl1 tc1 = new();
             TestControl2 tc2 = new();
 
-
             FrameworkPropertyMetadata metadata1 = TestControl1.TestProperty.GetMetadata(tc1) as FrameworkPropertyMetadata;
+            // All can be overriden from false to true.
+            // These can't be overriden from true to false:
+            Debug.WriteLine($"\r\nAffectsArrange = {metadata1.AffectsArrange}");
+            Debug.WriteLine($"AffectsMeasure = {metadata1.AffectsMeasure}");
+            Debug.WriteLine($"AffectsParentArrange = {metadata1.AffectsParentArrange}");
+            Debug.WriteLine($"AffectsParentMeasure = {metadata1.AffectsParentMeasure}");
+            Debug.WriteLine($"AffectsRender = {metadata1.AffectsRender}");
+            Debug.WriteLine($"BindsTwoWayByDefault = {metadata1.BindsTwoWayByDefault}");
+            Debug.WriteLine($"IsNotDataBindable = {metadata1.IsNotDataBindable}");
+            // These can be overriden from true to false:
+            Debug.WriteLine($"Inherits = {metadata1.Inherits}");
+            Debug.WriteLine($"Journal = {metadata1.Journal}");
+            Debug.WriteLine($"OverridesInheritanceBehavior = {metadata1.OverridesInheritanceBehavior}");
+            Debug.WriteLine($"SubPropertiesDoNotAffectRender = {metadata1.SubPropertiesDoNotAffectRender}\r\n");
+
             FrameworkPropertyMetadata metadata2 = TestControl2.TestProperty.GetMetadata(tc2) as FrameworkPropertyMetadata;
+            // All can be overriden from false to true.
+            // These can't be overriden from true to false:
+            Debug.WriteLine($"AffectsArrange = {metadata2.AffectsArrange}");
+            Debug.WriteLine($"AffectsMeasure = {metadata2.AffectsMeasure}");
+            Debug.WriteLine($"AffectsParentArrange = {metadata2.AffectsParentArrange}");
+            Debug.WriteLine($"AffectsParentMeasure = {metadata2.AffectsParentMeasure}");
+            Debug.WriteLine($"AffectsRender = {metadata2.AffectsRender}");
+            Debug.WriteLine($"BindsTwoWayByDefault = {metadata2.BindsTwoWayByDefault}");
+            Debug.WriteLine($"IsNotDataBindable = {metadata2.IsNotDataBindable}");
+            // These can be overriden from true to false:
+            Debug.WriteLine($"Inherits = {metadata2.Inherits}");
+            Debug.WriteLine($"Journal = {metadata2.Journal}");
+            Debug.WriteLine($"OverridesInheritanceBehavior = {metadata2.OverridesInheritanceBehavior}");
+            Debug.WriteLine($"SubPropertiesDoNotAffectRender = {metadata2.SubPropertiesDoNotAffectRender}");
         }
 
         public class TestControl1 : ButtonBase
         {
-            public static FrameworkPropertyMetadata fpm = new();
+            public static FrameworkPropertyMetadata fpm;
 
             public TestControl1() : base() 
             {
             }
 
-            public static void Tester()
+            static TestControl1()
             {
+                // Change all the default values.
+                bool boolVal = true;
+                var fpm = new FrameworkPropertyMetadata(defaultValue: 1)
+                {
+                    AffectsArrange = boolVal,
+                    AffectsMeasure = boolVal,
+                    AffectsParentArrange = boolVal,
+                    AffectsParentMeasure = boolVal,
+                    AffectsRender = boolVal,
+                    BindsTwoWayByDefault = boolVal,
+                    Inherits = boolVal,
+                    IsAnimationProhibited = boolVal,
+                    IsNotDataBindable = boolVal,
+                    Journal = boolVal,
+                    OverridesInheritanceBehavior = boolVal,
+                    SubPropertiesDoNotAffectRender = boolVal
+                };
+
+                TestProperty =
+                DependencyProperty.Register(
+                    name: "Test",
+                    propertyType: typeof(int),
+                    ownerType: typeof(TestControl1),
+                    typeMetadata: fpm);
             }
 
             public int Test
@@ -40,21 +93,8 @@ namespace WpfApp1
                 set => SetValue(TestProperty, value);
             }
 
-            public static readonly DependencyProperty TestProperty =
-                DependencyProperty.Register(
-                    name: "Test",
-                    propertyType: typeof(int),
-                    ownerType: typeof(TestControl1),
-                    typeMetadata: fpm);
-
-            public static readonly DependencyProperty Test2Property =
-                DependencyProperty.Register(
-                    name: "Test2",
-                    propertyType: typeof(int),
-                    ownerType: typeof(TestControl1));
+            public static readonly DependencyProperty TestProperty;
         }
-
-        static PropertyMetadata newPropMetadata2 = new(defaultValue: 2);
 
         public class TestControl2 : TestControl1
         {
@@ -64,32 +104,24 @@ namespace WpfApp1
 
             static TestControl2()
             {
-                var fpm = new FrameworkPropertyMetadata(defaultValue: 1);
-                fpm.AffectsArrange = !fpm.AffectsArrange;
-                fpm.AffectsMeasure = !fpm.AffectsMeasure;
-                fpm.AffectsParentArrange = !fpm.AffectsParentArrange;
-                fpm.AffectsParentMeasure = !fpm.AffectsParentMeasure;
-                fpm.AffectsRender = !fpm.AffectsRender;
-                fpm.BindsTwoWayByDefault = !fpm.BindsTwoWayByDefault;
-                fpm.Inherits = !fpm.Inherits;
-                fpm.IsAnimationProhibited = !fpm.IsAnimationProhibited;
-                fpm.IsNotDataBindable = !fpm.IsNotDataBindable;
-                fpm.Journal = !fpm.Journal;
-                fpm.OverridesInheritanceBehavior = !fpm.OverridesInheritanceBehavior;
-                fpm.SubPropertiesDoNotAffectRender = !fpm.SubPropertiesDoNotAffectRender;
+                bool boolVal = false;
+                var fpm = new FrameworkPropertyMetadata(defaultValue: 2)
+                {
+                    AffectsArrange = boolVal,
+                    AffectsMeasure = boolVal,
+                    AffectsParentArrange = boolVal,
+                    AffectsParentMeasure = boolVal,
+                    AffectsRender = boolVal,
+                    BindsTwoWayByDefault = boolVal,
+                    Inherits = boolVal,
+                    IsAnimationProhibited = boolVal,
+                    IsNotDataBindable = boolVal,
+                    Journal = boolVal,
+                    OverridesInheritanceBehavior = boolVal,
+                    SubPropertiesDoNotAffectRender = boolVal
+                };
 
                 TestProperty.OverrideMetadata(typeof(TestControl2), fpm);
-            }
-        }
-        public class TestControl3 : TestControl1
-        {
-            public TestControl3() : base()
-            {
-            }
-
-            static TestControl3()
-            {
-                TestProperty.OverrideMetadata(typeof(TestControl2), newPropMetadata2);
             }
         }
     }
