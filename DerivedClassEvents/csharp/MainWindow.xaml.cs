66a9728313812bd1a0807c8d4d326a612e4fc41c
v-trisshores <86677757+v-trisshores@users.noreply.github.com>
2022-03-17 13:52:26 -0500

    Add new test project

---
+++ DerivedClassEvents/csharp/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CodeSampleCsharp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Debug output:
+        // OnKeyDown class handler triggered from ComponentWrapper.
+    }
+
+    public class ComponentWrapper : ComponentWrapperBase
+    {
+        protected override void OnKeyDown(System.Windows.Input.KeyEventArgs e)
+        {
+            Debug.WriteLine($"OnKeyDown class handler triggered from ComponentWrapper.");
+
+            //e.Handled = true;
+
+            //base.OnKeyDown(e);
+        }
+    }
+
+    public class ComponentWrapperBase : StackPanel
+    {
+        protected override void OnKeyDown(System.Windows.Input.KeyEventArgs e)
+        {
+            Debug.WriteLine($"OnKeyDown class handler triggered from ComponentWrapperBase.");
+
+            //e.Handled = true;
+
+            //base.OnKeyDown(e);
+        }
+    }
+}
