dfce0d9cca6a512e0993b0a10937e776f134a833
v-trisshores <86677757+v-trisshores@users.noreply.github.com>
2022-04-29 10:46:16 -0500

    Improve preview event handled test.
    This test shows that the bubbling event in the input event pair isn't raised when a preview event is handled.

--- PreviewEventHandled/csharp/MainWindow.xaml.cs
+++ PreviewEventHandled/csharp/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace CodeSampleCsharp
 {
@@ -11,80 +12,30 @@ namespace CodeSampleCsharp
         {
             InitializeComponent();
 
-            // Attach a handler on outerStackPanel that will be invoked by handled KeyDown events.
-            componentTextBox.AddHandler(KeyDownEvent, new RoutedEventHandler(componentTextBox_KeyDown_handled),
-                handledEventsToo: true);
+            // Attach a handler on StackPanel that will be invoked by handled KeyDown events.
+            textBox.AddHandler(KeyDownEvent, new KeyEventHandler(TextBox_KeyDown), handledEventsToo: true);
+            stackPanel.AddHandler(KeyDownEvent, new KeyEventHandler(TextBox_KeyDown), handledEventsToo: true);
         }
 
-        private void componentTextBox_KeyDown_handled(object sender, RoutedEventArgs e)
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
         {
             string senderName = ((FrameworkElement)sender).Name;
             string sourceName = ((FrameworkElement)e.Source).Name;
             string eventName = e.RoutedEvent.Name;
-            string handledEventsToo = e.Handled ? " Parameter handledEventsToo set to true." : "";
             Debug.WriteLine($"Instance handler attached to {senderName} " +
-                $"triggered by {eventName} event raised on {sourceName}.{handledEventsToo}");
+                $"triggered by {eventName} event raised on {sourceName}. handledEventsToo={e.Handled}");
         }
 
-        private void outerStackPanel_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
         {
             string senderName = ((FrameworkElement)sender).Name;
             string sourceName = ((FrameworkElement)e.Source).Name;
             string eventName = e.RoutedEvent.Name;
-            string handledEventsToo = e.Handled ? " Parameter handledEventsToo set to true." : "";
             Debug.WriteLine($"Instance handler attached to {senderName} " +
-                $"triggered by {eventName} event raised on {sourceName}.{handledEventsToo}");
+                $"triggered by {eventName} event raised on {sourceName}. handledEventsToo={e.Handled}");
 
-            e.Handled = true;
-            Debug.WriteLine($"Handled={e.Handled}.");
-        }
-
-        private void componentTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            string senderName = ((FrameworkElement)sender).Name;
-            string sourceName = ((FrameworkElement)e.Source).Name;
-            string eventName = e.RoutedEvent.Name;
-            string handledEventsToo = e.Handled ? " Parameter handledEventsToo set to true." : "";
-            Debug.WriteLine($"Instance handler attached to {senderName} " +
-                $"triggered by {eventName} event raised on {sourceName}.{handledEventsToo}");
-        }
-
-        private void componentWrapper_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            string senderName = ((FrameworkElement)sender).Name;
-            string sourceName = ((FrameworkElement)e.Source).Name;
-            string eventName = e.RoutedEvent.Name;
-            string handledEventsToo = e.Handled ? " Parameter handledEventsToo set to true." : "";
-            Debug.WriteLine($"Instance handler attached to {senderName} " +
-                $"triggered by {eventName} event raised on {sourceName}.{handledEventsToo}");
-        }
-    }
-
-    public class ComponentWrapper : StackPanel
-    {
-        static ComponentWrapper()
-        {
-            EventManager.RegisterClassHandler(typeof(ComponentWrapper), KeyDownEvent, new RoutedEventHandler(KeyDownHandler), handledEventsToo: true);
-        }
-
-        private static void KeyDownHandler(object sender, RoutedEventArgs e)
-        {
-            string senderName = ((FrameworkElement)sender).Name;
-            string sourceName = ((FrameworkElement)e.Source).Name;
-            string eventName = e.RoutedEvent.Name;
-            string handledEventsToo = e.Handled ? " Parameter handledEventsToo set to true." : "";
-            Debug.WriteLine($"Class static handler attached to {senderName} " +
-                $"triggered by {eventName} event raised on {sourceName}.{handledEventsToo}");
-        }
-
-        protected override void OnKeyDown(System.Windows.Input.KeyEventArgs e)
-        {
-            string senderName = "ComponentWrapper";
-            string sourceName = ((FrameworkElement)e.Source).Name;
-            string eventName = e.RoutedEvent.Name;
-            string handledEventsToo = e.Handled ? " Parameter handledEventsToo set to true." : "";
-            Debug.WriteLine($"Class virtual handler attached to {senderName} " +
-                $"triggered by {eventName} event raised on {sourceName}.{handledEventsToo}");
+            //e.Handled = true;
+            Debug.WriteLine($"Setting Handled={e.Handled}.");
         }
     }
 }
